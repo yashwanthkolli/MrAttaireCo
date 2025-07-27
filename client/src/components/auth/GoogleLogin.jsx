cd9459f03112228b0e7dbe290221189c40e979a7
yashwanthkolli <yashwanthkolli3271@gmail.com>
2025-07-27 13:21:32 +0530

    Google oAuth login

--- client/src/components/auth/GoogleLogin.jsx
+++ client/src/components/auth/GoogleLogin.jsx
@@ -1,14 +1,13 @@
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider, GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import { useNavigate } from 'react-router-dom';
 import API from '../../utils/api';
+import { FcGoogle } from 'react-icons/fc';
 
 const GoogleLoginButton = () => {
   const navigate = useNavigate();
 
-  return (
-    <GoogleOAuthProvider clientId={'244554571154-ppp0ve2f3v2ufqp4re0mhcqvii799v4q.apps.googleusercontent.com'}>
-      <GoogleLogin
-        onSuccess={async (credentialResponse) => {
+  const login = useGoogleLogin({
+    onSuccess: async (credentialResponse) => {
           try {
             // Send the credential to your backend
             const res = await window.open(
@@ -23,12 +22,41 @@ const GoogleLoginButton = () => {
           } catch (err) {
             console.error('Google login failed:', err);
           }
-        }}
-        onError={() => {
-          console.log('Google login failed');
-        }}
-      />
-    </GoogleOAuthProvider>
+        }, 
+    onError: () => {
+      console.log('Google login failed');
+    }
+  })
+
+  return (
+    // <GoogleOAuthProvider clientId={'244554571154-ppp0ve2f3v2ufqp4re0mhcqvii799v4q.apps.googleusercontent.com'}>
+    //   <GoogleLogin
+    //     onSuccess={async (credentialResponse) => {
+    //       try {
+    //         // Send the credential to your backend
+    //         const res = await window.open(
+    //             // `http://localhost:5000/api/v1/auth/google`,
+    //             'https://mrattireco.com/backend/api/v1/auth/google',
+    //             '_self' // Opens in same tab
+    //         )
+            
+    //         // Store token and redirect
+    //         localStorage.setItem('token', res.data.token);
+    //         navigate('/');
+    //       } catch (err) {
+    //         console.error('Google login failed:', err);
+    //       }
+    //     }}
+    //     onError={() => {
+    //       console.log('Google login failed');
+    //     }}
+    //   />
+      
+    // </GoogleOAuthProvider>
+    <button className='google-button' onClick={() => login()}>
+      <span className='icon'><FcGoogle /></span>
+      <span className='text'>Sign in with Google</span>
+    </button>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='244554571154-ppp0ve2f3v2ufqp4re0mhcqvii799v4q.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
