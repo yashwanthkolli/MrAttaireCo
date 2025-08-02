379622f6e09be9ec6d27e50b123620cb80b1848e
yashwanthkolli <yashwanthkolli3271@gmail.com>
2025-08-02 17:04:33 +0530

    RazorPay Integration

--- Backend/utils/countryDetection.js
+++ Backend/utils/countryDetection.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
-import { publicIpv4 } from 'public-ip';
+const publicIp = require('public-ip');
+const { publicIpv4 } = publicIp;
 const { cache } = require('../utils/cache');
 const countryConfig = require('../config/countryConfig');
 
